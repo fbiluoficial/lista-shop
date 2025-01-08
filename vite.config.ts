20091a42374d826dda25ac9e1bfeccd50b00b958
fbiluoficial <fekobilu@gmail.com>
2025-01-07 21:39:23 -0300

    Configurando GitHub Pages e corrigindo base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/lista-shop/',
   server: {
     port: 5144,
   },
-  base: process.env.VITE_BASE_PATH || '/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
